9ecde08c0da8b907798b562a2736132418aceef9
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-05 21:14:50 -0500

    Update constants.py

--- incident_scraper/utils/constants.py
+++ incident_scraper/utils/constants.py
@@ -1,7 +1,13 @@
-"""This file holds all constants used throughout the incident scraper package."""
+"""This file holds all constants used throughout the incident scraper
+package.
+"""
 
 # DateTime Constants
 TIMEZONE_CHICAGO = "America/Chicago"
+
+# Incident Page Constants
+BASE_UCPD_URL = "https://incidentreports.uchicago.edu/incidentReportArchive.php"
+UCPD_URL_REPORT_DATE = BASE_UCPD_URL + "?reportDate="
 
 # Scraper Constants
 REQUEST_DELAY = 0.2
