5d3911ae289c4f46879b63e70c71aea2d0815efc
Federico Domínguez Molina <fd.molina@outlook.com>
2023-07-12 17:17:15 -0500

    Begin building class

--- incident_scraper/scraper/UCPDScraper.py
+++ incident_scraper/scraper/UCPDScraper.py
@@ -8,7 +8,13 @@ from datetime import datetime, time
 from urllib.parse import urlparse
 
 # Global Variables
-BASE_UCPD_URL = "https://incidentreports.uchicago.edu/incidentReportArchive.php"
-UCPD_URL_REPORT_DATE = BASE_UCPD_URL + "?reportDate="
 
-TIMEZONE_CHICAGO = "America/Chicago"
+
+class UCPDScraper:
+    def __init__(self, REQUEST_DELAY=0.2):
+        self.BASE_UCPD_URL = (
+            "https://incidentreports.uchicago.edu/incidentReportArchive.php"
+        )
+        self.UCPD_URL_REPORT_DATE = self.BASE_UCPD_URL + "?reportDate="
+        self.TIMEZONE_CHICAGO = "America/Chicago"
+        self.REQUEST_DELAY = REQUEST_DELAY
