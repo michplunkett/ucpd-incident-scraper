24f649edc5cb9c9aa15bf868fbd64d64fea525e3
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-11 19:25:06 -0500

    Update datastore.py

--- incident_scraper/google/datastore.py
+++ incident_scraper/google/datastore.py
@@ -1,29 +1,29 @@
 """Contains code relating to the Google Cloud Platform Datastore service."""
+import os
+
 from google.cloud.datastore import Client
 
-
-def get_client():
-    """Get GCP client.
-
-    Returns
-    -------
-    datastore.Client
-        A GCP client that allows you to easily interact with the GCP Datastore.
-    """
-    return Client()
+project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
 
-def add_incident():
-    pass
+class Datastore:
+    """Create the client and access GCP datastore functionality."""
 
+    def __init__(self):
+        self.client = Client(project_id)
 
-def add_date_incident_list():
-    pass
+    def _add_date_incident_list(self):
+        """Add date key to Datastore if it does not already exist."""
+        pass
 
+    def _does_incident_exist(self):
+        """Check if incident is in datastore list for its respective date."""
+        pass
 
-def does_incident_exist():
-    pass
+    def add_incident(self):
+        """Add incident to datastore."""
+        pass
 
-
-def remove_incident():
-    pass
+    def remove_incident(self):
+        """Remove incident from datastore."""
+        pass
