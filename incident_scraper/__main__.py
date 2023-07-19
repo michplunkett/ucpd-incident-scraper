b2bdc0109297c7e5fa4827fc3a4dff3826567626
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-18 22:23:03 -0500

    Add CLI entry points (#13)
    ## Describe your changes
    Added `seed_datastore`, `three_days`, `five_days`, and `ten_days`
    commands.
    
    ## Checklist before requesting a review
    - [x] The code runs successfully.
    
    ```shell
    (ucpd-incident-scraper-py3.11) michaelp@MacBook-Air-5 ucpd-incident-scraper % make seed_datastore
    python -m incident_scraper seed
    Gonna get ALL the incidents
    (ucpd-incident-scraper-py3.11) michaelp@MacBook-Air-5 ucpd-incident-scraper % make three_days
    python -m incident_scraper days-back 3
    Gonna go 3 days back.
    (ucpd-incident-scraper-py3.11) michaelp@MacBook-Air-5 ucpd-incident-scraper % make five_days
    python -m incident_scraper days-back 5
    Gonna go 5 days back.
    (ucpd-incident-scraper-py3.11) michaelp@MacBook-Air-5 ucpd-incident-scraper % make ten_days
    python -m incident_scraper days-back 10
    Gonna go 10 days back.
    (ucpd-incident-scraper-py3.11) michaelp@MacBook-Air-5 ucpd-incident-scraper % ```

--- incident_scraper/__main__.py
+++ incident_scraper/__main__.py
@@ -1,4 +1,31 @@
 """Serves as the entry point for the project module."""
+import argparse
+
+from click import IntRange
+
+
+def main():
+    """Run the UCPD Incident Scraper."""
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest="command")
+
+    days_back = subparser.add_parser("days-back")
+    days_back.add_argument(
+        "days",
+        # The range is locked between 3 and 10.
+        type=IntRange(3, 10),
+        default=3,
+    )
+
+    subparser.add_parser("seed")
+
+    args = parser.parse_args()
+
+    if args.command == "days-back":
+        print(f"Gonna go {args.days} days back.")
+    elif args.command == "seed":
+        print("Gonna get ALL the incidents")
+
 
 if __name__ == "__main__":
-    print("oh, what up?")
+    main()
