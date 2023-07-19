a789d29241573dbe25de549e2138616433fc5415
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-18 23:36:18 -0500

    Create headers.py

---
+++ incident_scraper/scraper/headers.py
@@ -0,0 +1,26 @@
+"""Contains the headers class."""
+import json
+import os
+import random
+
+
+class Headers:
+    """Class that contains all headers in external header file."""
+
+    def __init__(self):
+        self.list_of_headers = []
+        self._load_headers_file()
+
+    def _load_headers_file(self):
+        """Load the list of headers."""
+        path = (
+            os.getcwd().replace("\\", "/")
+            + "/incident_scraper/data/http_headers.json"
+        )
+        self.list_of_headers = json.load(path)
+
+    def get_random_header(self):
+        """Use random number generator to get random header from list."""
+        return self.list_of_headers[
+            random.randint(0, len(self.list_of_headers))
+        ]
