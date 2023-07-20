bb67bb06dfdac4d18c41cef39fbca9cc20c4d78d
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-19 23:06:16 -0500

    Update incident.py

--- incident_scraper/models/incident.py
+++ incident_scraper/models/incident.py
@@ -1,60 +1,67 @@
 """Contains the Incident model and all of its associated information."""
 
 from datetime import datetime
-from typing import Any
 
 import pytz
-from jsonic import Serializable
-
-from incident_scraper.utils.constants import (
-    TIMEZONE_CHICAGO,
-    UCPD_DATE_FORMAT,
-    UCPD_MDY_DATE_FORMAT,
+from google.cloud.ndb import (
+    DateTimeProperty,
+    FloatProperty,
+    Model,
+    StringProperty,
 )
 
+from incident_scraper.utils.constants import TIMEZONE_CHICAGO, UCPD_DATE_FORMAT
 
-class Incident(Serializable):
+
+class Incident(Model):
     """Standard data structure for recovered UCPD incidents."""
-
-    ucpd_id: str
-    incident: str
-    location: str
-    reported: str
-    reported_date: Any
-    occurred: str
-    comments: str
-    disposition: str
-    validated_location: str
-    validated_latitude: float
-    validate_longitude: float
 
     def __init__(self, gcp_response: dict = None, scrape_response: dict = None):
         super().__init__()
         if gcp_response:
-            self.ucpd_id = gcp_response["ucpd_id"]
-            self.incident = gcp_response["incident"]
-            self.location = gcp_response["location"]
-            self.reported = gcp_response["reported"]
-            self.reported_date = datetime.strptime(
-                gcp_response["reported_date"], UCPD_DATE_FORMAT
-            ).date()
-            self.occurred = gcp_response["occurred"]
-            self.comments = gcp_response["comments"]
-            self.disposition = gcp_response["disposition"]
-            self.validated_location = gcp_response["validated_location"]
-            self.validated_latitude = float(gcp_response["validated_latitude"])
-            self.validate_longitude = float(gcp_response["validate_longitude"])
+            self.ucpd_id = StringProperty(gcp_response["ucpd_id"])
+            self.incident = StringProperty(gcp_response["incident"])
+            self.location = StringProperty(gcp_response["location"])
+            self.reported = StringProperty(gcp_response["reported"])
+            self.reported_date = DateTimeProperty(
+                datetime.strptime(
+                    gcp_response["reported_date"], UCPD_DATE_FORMAT
+                )
+            )
+            self.occurred = StringProperty(gcp_response["occurred"])
+            self.comments = StringProperty(gcp_response["comments"])
+            self.disposition = StringProperty(gcp_response["disposition"])
+            self.validated_location = StringProperty(
+                gcp_response["validated_location"]
+            )
+            self.validated_latitude = FloatProperty(
+                float(gcp_response["validated_latitude"])
+            )
+            self.validate_longitude = FloatProperty(
+                float(gcp_response["validate_longitude"])
+            )
         elif scrape_response:
-            self.ucpd_id = scrape_response["UCPD_ID"]
-            self.incident = scrape_response["Incident"]
-            self.location = scrape_response["Location"]
-            self.reported = scrape_response["Reported"]
-            self.reported_date = datetime.strptime(
-                scrape_response["Reported"], UCPD_DATE_FORMAT
-            ).strftime(UCPD_MDY_DATE_FORMAT)
-            self.occurred = scrape_response["Occurred"]
-            self.comments = scrape_response["Comments / Nature of Fire"]
-            self.disposition = scrape_response["Disposition"]
+            self.ucpd_id = StringProperty(scrape_response["UCPD_ID"])
+            self.incident = StringProperty(scrape_response["Incident"])
+            self.location = StringProperty(scrape_response["Location"])
+            self.reported = StringProperty(scrape_response["Reported"])
+            self.reported_date = DateTimeProperty(
+                datetime.strptime(scrape_response["Reported"], UCPD_DATE_FORMAT)
+            )
+            self.occurred = StringProperty(scrape_response["Occurred"])
+            self.comments = StringProperty(
+                scrape_response["Comments / Nature of Fire"]
+            )
+            self.disposition = StringProperty(scrape_response["Disposition"])
+            self.validated_location = StringProperty(
+                scrape_response["validated_location"]
+            )
+            self.validated_latitude = FloatProperty(
+                float(scrape_response["validated_latitude"])
+            )
+            self.validate_longitude = FloatProperty(
+                float(scrape_response["validate_longitude"])
+            )
 
     def _date_str_to_iso_format(self, date_str: str):
         """Take a date and return in it a localized ISO format."""
@@ -71,10 +78,6 @@ class Incident(Serializable):
             .astimezone(pytz.timezone(TIMEZONE_CHICAGO))
             .strftime(self.UCPD_DOY_DATE_FORMAT)
         )
-
-    def get_parsed_location(self):
-        """Parse UCPD sourced location for validation."""
-        return self.location.split(" (")[0]
 
     def set_validated_location(self, census_response: str):
         """Set the validated locations properties."""
