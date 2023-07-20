120ca27bae7843e6e5fca1ffd2e723c3e1f5d1a6
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-20 00:24:26 -0500

    Update incident.py

--- incident_scraper/models/incident.py
+++ incident_scraper/models/incident.py
@@ -3,12 +3,7 @@
 from datetime import datetime
 
 import pytz
-from google.cloud.ndb import (
-    DateTimeProperty,
-    FloatProperty,
-    Model,
-    StringProperty,
-)
+from google.cloud.ndb import DateProperty, GeoPtProperty, Model, StringProperty
 
 from incident_scraper.utils.constants import TIMEZONE_CHICAGO, UCPD_DATE_FORMAT
 
@@ -16,76 +11,39 @@ from incident_scraper.utils.constants import TIMEZONE_CHICAGO, UCPD_DATE_FORMAT
 class Incident(Model):
     """Standard data structure for recovered UCPD incidents."""
 
-    def __init__(self, gcp_response: dict = None, scrape_response: dict = None):
-        super().__init__()
-        if gcp_response:
-            self.ucpd_id = StringProperty(gcp_response["ucpd_id"])
-            self.incident = StringProperty(gcp_response["incident"])
-            self.location = StringProperty(gcp_response["location"])
-            self.reported = StringProperty(gcp_response["reported"])
-            self.reported_date = DateTimeProperty(
-                datetime.strptime(
-                    gcp_response["reported_date"], UCPD_DATE_FORMAT
-                )
-            )
-            self.occurred = StringProperty(gcp_response["occurred"])
-            self.comments = StringProperty(gcp_response["comments"])
-            self.disposition = StringProperty(gcp_response["disposition"])
-            self.validated_location = StringProperty(
-                gcp_response["validated_location"]
-            )
-            self.validated_latitude = FloatProperty(
-                float(gcp_response["validated_latitude"])
-            )
-            self.validate_longitude = FloatProperty(
-                float(gcp_response["validate_longitude"])
-            )
-        elif scrape_response:
-            self.ucpd_id = StringProperty(scrape_response["UCPD_ID"])
-            self.incident = StringProperty(scrape_response["Incident"])
-            self.location = StringProperty(scrape_response["Location"])
-            self.reported = StringProperty(scrape_response["Reported"])
-            self.reported_date = DateTimeProperty(
-                datetime.strptime(scrape_response["Reported"], UCPD_DATE_FORMAT)
-            )
-            self.occurred = StringProperty(scrape_response["Occurred"])
-            self.comments = StringProperty(
-                scrape_response["Comments / Nature of Fire"]
-            )
-            self.disposition = StringProperty(scrape_response["Disposition"])
-            self.validated_location = StringProperty(
-                scrape_response["validated_location"]
-            )
-            self.validated_latitude = FloatProperty(
-                float(scrape_response["validated_latitude"])
-            )
-            self.validate_longitude = FloatProperty(
-                float(scrape_response["validate_longitude"])
-            )
+    ucpd_id = StringProperty(indexed=True)
+    incident = StringProperty(indexed=True)
+    reported = StringProperty()
+    reported_date = DateProperty(indexed=True)
+    occurred = StringProperty()
+    comments = StringProperty()
+    disposition = StringProperty()
+    location = StringProperty()
+    validated_address = StringProperty()
+    validated_location = GeoPtProperty()
 
-    def _date_str_to_iso_format(self, date_str: str):
-        """Take a date and return in it a localized ISO format."""
-        return (
-            datetime.strptime(date_str, self.UCPD_DATE_FORMAT)
-            .astimezone(pytz.timezone(TIMEZONE_CHICAGO))
-            .isoformat()
-        )
 
-    def _date_str_to_datastore_format(self, date_str: str):
-        """Take a date and return it as a localized MM/DD/YYYY format."""
-        return (
-            datetime.strptime(date_str, self.UCPD_DATE_FORMAT)
-            .astimezone(pytz.timezone(TIMEZONE_CHICAGO))
-            .strftime(self.UCPD_DOY_DATE_FORMAT)
-        )
+def set_validated_location(scrape_response: dict, census_response: str):
+    """Set the validated locations properties."""
+    if not census_response:
+        return False
 
-    def set_validated_location(self, census_response: str):
-        """Set the validated locations properties."""
-        if not census_response:
-            return False
+    scrape_response["ValidatedAddress"] = census_response[0]
+    scrape_response["ValidatedLatitude"] = census_response[1]
+    scrape_response["ValidatedLongitude"] = census_response[2]
 
-        self.validated_location = census_response[0]
-        self.validated_latitude = census_response[1]
-        self.validate_longitude = census_response[2]
+    return True
 
-        return True
+
+def date_str_to_iso_format(date_str: str):
+    """Take a date string and return in it a localized ISO format."""
+    return (
+        datetime.strptime(date_str, UCPD_DATE_FORMAT)
+        .astimezone(pytz.timezone(TIMEZONE_CHICAGO))
+        .isoformat()
+    )
+
+
+def date_str_to_date_format(date_str: str):
+    """Take a date string and return in it a localized date format."""
+    return datetime.strptime(date_str, UCPD_DATE_FORMAT).date()
