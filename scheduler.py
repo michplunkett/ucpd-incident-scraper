be5a4458191f57e7c578cdc293784c88e223259f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-13 12:23:54 -0500

    Set correct cron time

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,8 @@ from incident_scraper.__main__ import update_records
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job("cron", day_of_week="mon,wed,sat", hour=17, minute=21)
+# The time below is in UTC
+@scheduler.scheduled_job("cron", day_of_week="mon,wed,sat", hour=9)
 def run_scraper():
     """Run the scraper at the above interval."""
     update_records()
