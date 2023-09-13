15885a567cbbd7229945d9eb27840a715adb1d06
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-13 10:11:24 -0500

    Add click and update time

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ from incident_scraper.__main__ import update_records
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job("cron", day_of_week="tue,wed,sat", hour=15)
+@scheduler.scheduled_job("cron", day_of_week="mon,wed,sat", hour=15, minute=15)
 def run_scraper():
     """Run the scraper at the above interval."""
     update_records()
