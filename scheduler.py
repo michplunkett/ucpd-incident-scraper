d05570ae66aad093f54ad019574ab1144f05787e
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-13 12:19:47 -0500

    Verifying GCP sync with Heroku

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ from incident_scraper.__main__ import update_records
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job("cron", day_of_week="mon,wed,sat", hour=17, minute=17)
+@scheduler.scheduled_job("cron", day_of_week="mon,wed,sat", hour=17, minute=21)
 def run_scraper():
     """Run the scraper at the above interval."""
     update_records()
