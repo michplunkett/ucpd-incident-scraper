61805a408dc528862ff940f0172704c122307a4c
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-12 23:39:14 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,4 @@
+"""The python equivalent of a cron file."""
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from incident_scraper.__main__ import update_records
@@ -7,6 +8,7 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job("cron", day_of_week="tue,wed,sat", hour=4)
 def run_scraper():
+    """Run the scraper at the above interval."""
     update_records()
 
 
