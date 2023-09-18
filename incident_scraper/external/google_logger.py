1f09ece3dba22fc7e0adae700be688e74c62abc5
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-17 20:29:09 -0500

    Update google_logger.py

--- incident_scraper/external/google_logger.py
+++ incident_scraper/external/google_logger.py
@@ -6,19 +6,22 @@ import sys
 import google.cloud.logging as gcp_logging
 from google.oauth2 import service_account
 
-from incident_scraper.utils.constants import ENV_CREDENTIALS, ENV_PROJECT_ID
+from incident_scraper.utils.constants import (
+    ENV_GCP_CREDENTIALS,
+    ENV_GCP_PROJECT_ID,
+)
 
 
 def init_logger():
     """Set logger defaults."""
-    if ENV_CREDENTIALS.endswith(".json"):
-        logging_client = gcp_logging.Client(project=ENV_PROJECT_ID)
+    if ENV_GCP_CREDENTIALS.endswith(".json"):
+        logging_client = gcp_logging.Client(project=ENV_GCP_PROJECT_ID)
     else:
         credentials = service_account.Credentials.from_service_account_info(
-            json.loads(ENV_CREDENTIALS)
+            json.loads(ENV_GCP_CREDENTIALS)
         )
         logging_client = gcp_logging.Client(
-            credentials=credentials, project=ENV_PROJECT_ID
+            credentials=credentials, project=ENV_GCP_PROJECT_ID
         )
 
     logging_client.setup_logging(log_level=logging.INFO)
