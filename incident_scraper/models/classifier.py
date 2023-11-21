71717b9b151c3a771f872c86bcf1e438e3b37d17
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-21 02:01:57 -0600

    Update classifier.py

--- incident_scraper/models/classifier.py
+++ incident_scraper/models/classifier.py
@@ -1,14 +1,18 @@
 import numpy as np
+import pandas as pd
 import polars as pl
 import polars.selectors as cs
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
 
 from incident_scraper.utils.constants import (
     TIMEZONE_KEY_CHICAGO,
     UCPD_MDY_KEY_DATE_FORMAT,
 )
-
 
 INCIDENT_FILE = "incident_dump.csv"
 KEY_INCIDENT = "incident"
@@ -28,7 +32,7 @@ class Classifier:
             .str.split(",")
             .cast(pl.List(pl.Float64)),
         )
-        self._vectorizer = TfidfVectorizer(max_features=5000, max_df=0.85)
+        self._vectorizer = CountVectorizer(analyzer="word", max_features=5000, min_df=0.85)
         self._incidents = self._create_parsed_list()
         self._incidents.sort()
 
@@ -65,11 +69,75 @@ class Classifier:
                 ],
             ).drop("")
 
-        X = self._df.select("comments")
-        y = np.asarray(self._df.select(cs.by_name(self._incidents)))
+
+        X = self._df.select("comments").to_pandas(
+            use_pyarrow_extension_array=True
+        )
+        y = np.asarray(self._df.select(cs.by_name(*self._incidents)))
         self._vectorizer.fit(X)
+        print(X)
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.30
+            X, y, test_size=0.20, random_state=334
         )
         self._vectorizer.transform(X_train)
         self._vectorizer.transform(X_test)
+
+        train_ratio = 0.70
+        validation_ratio = 0.20
+        test_ratio = 0.10
+
+        # performing train test split
+        x_train, x_test, y_train, y_test = train_test_split(
+            X, y, test_size=1 - train_ratio)
+
+        # performing test validation split
+        x_val, x_test, y_val, y_test = train_test_split(
+            x_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio))
+
+        # initializing all the base model objects with default parameters
+        model_1 = LinearRegression()
+        model_2 = XGBRegressor()
+        model_3 = RandomForestRegressor()
+
+        # training all the model on the train dataset
+
+        # training first model
+        model_1.fit(x_train, y_train)
+        val_pred_1 = model_1.predict(x_val)
+        test_pred_1 = model_1.predict(x_test)
+
+        # converting to dataframe
+        val_pred_1 = pd.DataFrame(val_pred_1)
+        test_pred_1 = pd.DataFrame(test_pred_1)
+
+        # training second model
+        model_2.fit(x_train, y_train)
+        val_pred_2 = model_2.predict(x_val)
+        test_pred_2 = model_2.predict(x_test)
+
+        # converting to dataframe
+        val_pred_2 = pd.DataFrame(val_pred_2)
+        test_pred_2 = pd.DataFrame(test_pred_2)
+
+        # training third model
+        model_3.fit(x_train, y_train)
+        val_pred_3 = model_1.predict(x_val)
+        test_pred_3 = model_1.predict(x_test)
+
+        # converting to dataframe
+        val_pred_3 = pd.DataFrame(val_pred_3)
+        test_pred_3 = pd.DataFrame(test_pred_3)
+
+        # concatenating validation dataset along with all the predicted validation data (meta features)
+        df_val = pd.concat([x_val, val_pred_1, val_pred_2, val_pred_3], axis=1)
+        df_test = pd.concat([x_test, test_pred_1, test_pred_2, test_pred_3], axis=1)
+
+        # making the final model using the meta features
+        final_model = LinearRegression()
+        final_model.fit(df_val, y_val)
+
+        # getting the final output
+        pred_final = final_model.predict(df_test)
+
+        #printing the mean squared error between real value and predicted value
+        print(mean_squared_error(y_test, pred_final))
