208553f5f6d5c428f5580da989ba4cd42b6faccf
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-26 00:11:04 -0600

    Update functions.py

--- incident_scraper/utils/functions.py
+++ incident_scraper/utils/functions.py
@@ -98,3 +98,15 @@ def parse_scraped_incident_timestamp(i: dict) -> Optional[str]:
         break
 
     return result
+
+
+def determine_season(test_date: datetime) -> str:
+    test_date_tuple = (test_date.month, test_date.day)
+    if (3, 1) <= test_date_tuple < (5, 31):
+        return "Spring"
+    elif (6, 1) <= test_date_tuple < (8, 31):
+        return "Summer"
+    elif (9, 1) <= test_date_tuple < (12, 1):
+        return "Fall"
+    else:
+        return "Winter"
