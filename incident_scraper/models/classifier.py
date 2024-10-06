8a25bbea99f0cd42602bdc86786cf9594f4bb686
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-10-06 15:46:26 -0500

    Update classifier.py

--- incident_scraper/models/classifier.py
+++ incident_scraper/models/classifier.py
@@ -42,7 +42,7 @@ TEXT_NORMALIZING_FUNCTIONS = [
 
 class Classifier:
     def __init__(self, build_model: bool = False):
-        self.__vectorizer = TfidfVectorizer(
+        self._vectorizer = TfidfVectorizer(
             lowercase=True,
             max_features=1000,
             stop_words="english",
@@ -50,7 +50,7 @@ class Classifier:
         )
 
         if build_model:
-            self.__df = (
+            self._df = (
                 pl.read_csv(
                     f"./{INCIDENT_FILE}",
                 )
@@ -71,14 +71,14 @@ class Classifier:
                     )
                 )
             )
-            self.__unique_types = self.__create_unique_type_list()
-            self.__clean_data()
-            self.__model = None
+            self._unique_types = self._create_unique_type_list()
+            self._clean_data()
+            self._model = None
         else:
-            self.__load_model()
+            self._load_model()
 
     @staticmethod
-    def __reset_category_casing(category: str) -> str:
+    def _reset_category_casing(category: str) -> str:
         category = custom_title_case(category)
         replacements: [tuple] = [
             ("Dui", "DUI"),
@@ -92,8 +92,8 @@ class Classifier:
 
         return category
 
-    def __create_unique_type_list(self) -> [str]:
-        incidents = self.__df[KEY_INCIDENT_TYPE].to_list()
+    def _create_unique_type_list(self) -> [str]:
+        incidents = self._df[KEY_INCIDENT_TYPE].to_list()
         incident_set = set()
         for element in incidents:
             if "/" in element:
@@ -109,9 +109,9 @@ class Classifier:
         incident_list.sort()
         return incident_list
 
-    def __clean_data(self) -> None:
-        for i in self.__unique_types:
-            self.__df = self.__df.with_columns(
+    def _clean_data(self) -> None:
+        for i in self._unique_types:
+            self._df = self._df.with_columns(
                 pl.col(KEY_INCIDENT_TYPE)
                 .str.to_lowercase()
                 .str.split(" / ")
@@ -125,43 +125,43 @@ class Classifier:
                 .alias(i),
             )
 
-        self.__df = self.__df.filter(
+        self._df = self._df.filter(
             pl.col(KEY_INCIDENT_TYPE) != INCIDENT_TYPE_INFO
         )
 
         min_type_cnt = (
-            self.__df.select(pl.sum(*self.__unique_types))
+            self._df.select(pl.sum(*self._unique_types))
             .transpose(include_header=True)
             .filter(pl.col("column_0") > MINIMUM_TYPE_FREQUENCY)
         )
 
         min_type_cnt_list = [t[0] for t in min_type_cnt.select("column").rows()]
 
-        self.__df = self.__df.select(
+        self._df = self._df.select(
             KEY_COMMENTS, KEY_INCIDENT_TYPE, *min_type_cnt_list
         )
 
-        self.__df = self.__df.to_pandas(use_pyarrow_extension_array=True)
+        self._df = self._df.to_pandas(use_pyarrow_extension_array=True)
 
-    def __train(self) -> None:
-        X = self.__df[KEY_COMMENTS].tolist()
-        y = np.asarray(self.__df[self.__df.columns[2:]], dtype=int)
-        self.__vectorizer.fit(X)
+    def _train(self) -> None:
+        X = self._df[KEY_COMMENTS].tolist()
+        y = np.asarray(self._df[self._df.columns[2:]], dtype=int)
+        self._vectorizer.fit(X)
 
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.35
         )
 
-        X_train_tfidf = self.__vectorizer.transform(X_train)
-        X_test_tfidf = self.__vectorizer.transform(X_test)
+        X_train_tfidf = self._vectorizer.transform(X_train)
+        X_test_tfidf = self._vectorizer.transform(X_test)
 
-        self.__model = MultiOutputClassifier(
+        self._model = MultiOutputClassifier(
             XGBClassifier(
                 eta=0.2, max_depth=10, n_estimators=100, booster="gbtree"
             )
         ).fit(X_train_tfidf, y_train)
 
-        prediction = self.__model.predict(X_test_tfidf)
+        prediction = self._model.predict(X_test_tfidf)
         accuracy = accuracy_score(y_test, prediction)
         precision = precision_score(
             y_test, prediction, average="micro", zero_division=0.0
@@ -173,38 +173,38 @@ class Classifier:
         logging.info(f"Precision Score: {precision}")
         logging.info(f"Recall Score: {recall}")
 
-    def __save_model(self) -> None:
-        pickle.dump(self.__model, open(SAVED_MODEL_LOCATION, mode="wb"))
+    def _save_model(self) -> None:
+        pickle.dump(self._model, open(SAVED_MODEL_LOCATION, mode="wb"))
         pickle.dump(
-            self.__vectorizer, open(SAVED_VECTORIZER_LOCATION, mode="wb")
+            self._vectorizer, open(SAVED_VECTORIZER_LOCATION, mode="wb")
         )
-        pickle.dump(self.__unique_types, open(SAVED_TYPES_LOCATION, mode="wb"))
+        pickle.dump(self._unique_types, open(SAVED_TYPES_LOCATION, mode="wb"))
 
-    def __load_model(self) -> None:
+    def _load_model(self) -> None:
         if os.path.isfile(SAVED_MODEL_LOCATION) and os.path.isfile(
             SAVED_VECTORIZER_LOCATION
         ):
-            self.__model = pickle.load(open(SAVED_MODEL_LOCATION, mode="rb"))
-            self.__vectorizer = pickle.load(
+            self._model = pickle.load(open(SAVED_MODEL_LOCATION, mode="rb"))
+            self._vectorizer = pickle.load(
                 open(SAVED_VECTORIZER_LOCATION, mode="rb")
             )
-            self.__unique_types = pickle.load(
+            self._unique_types = pickle.load(
                 open(SAVED_TYPES_LOCATION, mode="rb")
             )
 
     def train_and_save(self) -> None:
-        self.__train()
-        self.__save_model()
+        self._train()
+        self._save_model()
 
     def get_predicted_incident_type(self, comment: str) -> Optional[str]:
         comment = reduce(lambda t, f: f(t), TEXT_NORMALIZING_FUNCTIONS, comment)
-        vectorized_comment = self.__vectorizer.transform([comment])
-        prediction = self.__model.predict(vectorized_comment).tolist()[0]
+        vectorized_comment = self._vectorizer.transform([comment])
+        prediction = self._model.predict(vectorized_comment).tolist()[0]
         label_indexes = [
             i for i in range(len(prediction)) if prediction[i] == 1
         ]
         if label_indexes:
-            labels = [self.__unique_types[i] for i in label_indexes]
-            return self.__reset_category_casing(" / ".join(labels))
+            labels = [self._unique_types[i] for i in label_indexes]
+            return self._reset_category_casing(" / ".join(labels))
 
         return None
